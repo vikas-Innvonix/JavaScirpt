782e232bda6281bdb12f651dd1e196263e69c0cc
vikas-Innvonix <vikas.innvonix@gmail.com>
2022-06-21 18:18:38 +0530

    Change In File

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,14 +1,13 @@
-
+//Finding Phibonacci For given Number
 const fibo = (n)=>{
     let i=0,j=1;
-    let arr = [i,j]
+    let arr = [i,j];
     for(let k=2;k<n;++k){
-        let fibo = i+j;
+	let fibo = i+j;
         arr.push(fibo);
         i = j;
         j = fibo;
     }
     return arr;
 }
-
 export default fibo;
